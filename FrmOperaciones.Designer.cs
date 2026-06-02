c26a20d437081413708b86ab60069cd3c787926a
David Gonzalez <davidg.050906@gmail.com>
2026-06-02 12:40:41 -0400

    feat: FrmOperaciones y Reportes con formato moneda y validacion

---
+++ FrmOperaciones.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SistemaGestionInmobiliaria
+{
+    partial class FrmOperaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.cmbInmuebles = new System.Windows.Forms.ComboBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnVerReportes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbClientes.FormattingEnabled = true;
+            this.cmbClientes.Location = new System.Drawing.Point(476, 96);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(300, 28);
+            this.cmbClientes.TabIndex = 0;
+            // 
+            // cmbInmuebles
+            // 
+            this.cmbInmuebles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbInmuebles.FormattingEnabled = true;
+            this.cmbInmuebles.Location = new System.Drawing.Point(476, 241);
+            this.cmbInmuebles.Name = "cmbInmuebles";
+            this.cmbInmuebles.Size = new System.Drawing.Size(300, 28);
+            this.cmbInmuebles.TabIndex = 2;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(301, 371);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(186, 44);
+            this.btnProcesar.TabIndex = 3;
+            this.btnProcesar.Text = "Procesar Contrato";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "lnmueble";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnVerReportes
+            // 
+            this.btnVerReportes.Location = new System.Drawing.Point(618, 382);
+            this.btnVerReportes.Name = "btnVerReportes";
+            this.btnVerReportes.Size = new System.Drawing.Size(131, 33);
+            this.btnVerReportes.TabIndex = 6;
+            this.btnVerReportes.Text = "Ver reportes";
+            this.btnVerReportes.UseVisualStyleBackColor = true;
+            this.btnVerReportes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmOperaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerReportes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.cmbInmuebles);
+            this.Controls.Add(this.cmbClientes);
+            this.Name = "FrmOperaciones";
+            this.Text = "FrmOperaciones";
+            this.Load += new System.EventHandler(this.FrmOperaciones_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbClientes;
+        private System.Windows.Forms.ComboBox cmbInmuebles;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnVerReportes;
+    }
+}
---
+++ FrmOperaciones.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestionInmobiliaria
+{
+    public partial class FrmOperaciones : Form
+    {
+        // Cree dos lists para simular los datos con propiedade
+        private List<object> listaClientesSimulados = new List<object>();
+        private List<object> listaInmueblesSimulados = new List<object>();
+
+        public FrmOperaciones()
+        {
+            InitializeComponent();
+            CargarDatosLocales();
+        }
+
+        private void FrmOperaciones_Load(object sender, EventArgs e)
+        {
+            CargarListasDesplegables();
+        }
+
+        private void CargarDatosLocales()
+        {
+            // Clientes
+            listaClientesSimulados.Add(new { NombreCompleto = "David Rodríguez" });
+            listaClientesSimulados.Add(new { NombreCompleto = "María Alejandra" });
+            listaClientesSimulados.Add(new { NombreCompleto = "Juan Pérez" });
+
+            // Inmuebles
+            listaInmueblesSimulados.Add(new { Direccion = "Apartamento Playa El Ángel - Pampatar" });
+            listaInmueblesSimulados.Add(new { Direccion = "Local Comercial Mercado de Conejeros" });
+            listaInmueblesSimulados.Add(new { Direccion = "Quinta Av. Bolívar - Porlamar" });
+        }
+
+        private void CargarListasDesplegables()
+        {
+            try
+            {
+                // conecte clienes con cmbClientes
+                cmbClientes.DataSource = null;
+                cmbClientes.DataSource = listaClientesSimulados;
+                cmbClientes.DisplayMember = "NombreCompleto";
+
+                // Conecte los inmuebles con cmbInmuebles
+                cmbInmuebles.DataSource = null;
+                cmbInmuebles.DataSource = listaInmueblesSimulados;
+                cmbInmuebles.DisplayMember = "Direccion";
+
+               
+                cmbClientes.SelectedIndex = -1;
+                cmbInmuebles.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar las listas: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            bool tieneError = false;
+
+            if (cmbClientes.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(cmbClientes, "¡Requerido! Debe seleccionar un cliente.");
+                tieneError = true;
+            }
+
+            if (cmbInmuebles.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(cmbInmuebles, "¡Requerido! Debe seleccionar un inmueble.");
+                tieneError = true;
+            }
+
+            if (tieneError)
+            {
+                MessageBox.Show("Por favor, complete las selecciones marcadas en rojo.",
+                                "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // registro que el usuario selecciona
+                var inmuebleSeleccionado = cmbInmuebles.SelectedItem;
+                var clienteSeleccionado = cmbClientes.SelectedItem;
+
+                // ====================================================================
+                // MODIFICACIÓN: AGREGAR EL CONTRATO A LA LISTA ESTÁTICA DE REPORTES
+                // ====================================================================
+                // Usamos dynamic para leer las propiedades de los objetos anónimos simulados
+                dynamic clienteData = clienteSeleccionado;
+                dynamic inmuebleData = inmuebleSeleccionado;
+
+                var nuevoContrato = new
+                {
+                    Cliente = clienteData.NombreCompleto,
+                    Inmueble = inmuebleData.Direccion,
+                    FechaRegistro = DateTime.Now.ToString("dd/MM/yyyy HH:mm")
+                };
+
+                FrmReportes.historialContratos.Add(nuevoContrato);
+                // ====================================================================
+
+                MessageBox.Show("¡Contrato generado con éxito y estado del inmueble actualizado!",
+                                "Operación Exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                // Remueve el inmueble de la lista local para simular que ya se vendió/alquiló
+                listaInmueblesSimulados.Remove(inmuebleSeleccionado);
+
+                CargarListasDesplegables();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al procesar el contrato: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FrmOperaciones_Load_1(object sender, EventArgs e)
+        {
+            CargarListasDesplegables();
+        }
+
+        private void btnProcesar_Click_1(object sender, EventArgs e)
+        {
+            btnProcesar_Click(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmReportes ventanaReportes = new FrmReportes();
+
+            ventanaReportes.ShowDialog();
+        }
+    }
+}
